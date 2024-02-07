afda93713f9a72b12c905553df480d6272363081
ArinoMichi <cutesyprogrammer@gmail.com>
2024-02-07 13:20:27 +0100

    MVC and ADO

---
+++ MvcCoreAdoNet/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult VistaMascotas()
+        {
+
+            List<Mascota> mascotas = ObtenerListaMascotas();
+            return View(mascotas);
+        }
+        private List<Mascota> ObtenerListaMascotas()
+        {
+            // Aquí puedes crear y retornar una lista de mascotas de ejemplo
+            List<Mascota> mascotas = new List<Mascota>
+        {
+            new Mascota("Max", 3, "Labrador"),
+            new Mascota("Luna", 2, "Golden Retriever"),
+            new Mascota("Rocky", 4, "Bulldog")
+        };
+
+            return mascotas;
+        }
+
+    }
+}
---
+++ MvcCoreAdoNet/Models/Mascota.cs
@@ -0,0 +1,16 @@
+﻿namespace MvcCoreAdoNet.Models
+{
+    public class Mascota
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Raza { get; set; }
+
+        public Mascota(string nombre, int edad, string raza)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Raza = raza;
+        }
+    }
+}
